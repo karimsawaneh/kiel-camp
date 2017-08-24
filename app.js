4693e28139fc7dff697f55d1bd3958c2013c3b3c
Karim <sakjrn@gmail.com>
2017-08-25 00:16:55 +0200

    Added the show page so that we can look up a particular campground. Some progress

--- app.js
+++ app.js
@@ -16,14 +16,16 @@ app.set('view engine', 'ejs');
 // Schema setup
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 var Campground = mongoose.model('Campground', campgroundSchema);
 
 // Campground.create(
 //     { 
 //         name: 'Steenbeker Creek', 
-//         image: 'https://cdn.pixabay.com/photo/2013/09/16/19/15/camp-182951__340.jpg' 
+//         image: 'https://cdn.pixabay.com/photo/2013/09/16/19/15/camp-182951__340.jpg',
+//         description: 'this is a beautiful creek, no bathrooms, no water. Beautiful creek!' 
 //     },   function(err, campground){
 //          if(err){
 //         console.log(err);
@@ -38,24 +40,25 @@ app.get('/', function(req, res){
     res.render('landing');
 }); 
 
-// campgrounds route
+// INDEX route - show all campgrounds
 app.get('/campgrounds', function(req, res){
     //Get all campgrounds from the db
     Campground.find({}, function(err, allCampgrounds){
         if(err) {
             console.log(err);
         } else {
-            res.render('Campgrounds', {campgrounds: allCampgrounds});
+            res.render('index', {campgrounds: allCampgrounds});
         }
     });
 });
 
-// New campground route
+// CREATE route - add new campground to DB
 app.post('/campgrounds', function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image:image};
+    var desc = req.body.description;
+    var newCampground = {name: name, image:image, description: desc};
     // create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err) {
@@ -67,9 +70,22 @@ app.post('/campgrounds', function(req, res){
     });
 });
 
-// new form route where we can add campgrounds
+// NEW route - show form where we can create campgrounds
 app.get('/campgrounds/new', function(req, res){
     res.render('new.ejs');
+});
+
+// SHOW route - shows info about a particular campground
+app.get('/campgrounds/:id', function(req, res){
+    // find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err) {
+            console.log(err);
+        } else {
+            // render show template with that campground
+            res.render('show', {campground: foundCampground});
+        }
+    });
 });
 
 // Express Server
