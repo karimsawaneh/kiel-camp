57d0f1b7ec0d0a3ac324699504ee2f744a216f5c
Karim <sakjrn@gmail.com>
2017-08-24 22:50:40 +0200

    Added mongoose so that we can save campgrounds to a database. Pieces falling into place here

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
 
+// connect mongoose
+mongoose.connect('mongodb://localhost/kiel_camp', ({
+    useMongoClient: true
+}));
 // bodyparser
 app.use(bodyParser.urlencoded({extended: true}));
 // setup view engine
 app.set('view engine', 'ejs');
 
+// Schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create(
+//     { 
+//         name: 'Steenbeker Creek', 
+//         image: 'https://cdn.pixabay.com/photo/2013/09/16/19/15/camp-182951__340.jpg' 
+//     },   function(err, campground){
+//          if(err){
+//         console.log(err);
+//     } else {
+//         console.log('NEWLY CREATED CAMPGROUND: ');
+//         console.log(campground);
+//     }
+// });
+
 // Landing page
 app.get('/', function(req, res){
     res.render('landing');
-});
-
-// temp
-var campgrounds = [
-    { name: 'Bootshafen Slope', image: 'https://cdn.pixabay.com/photo/2016/09/05/12/48/camping-1646504__340.jpg' },
-    { name: 'Steenbeker Creek', image: 'https://cdn.pixabay.com/photo/2013/09/16/19/15/camp-182951__340.jpg' },
-    { name: 'Bahnhoff Stadt', image: 'https://cdn.pixabay.com/photo/2017/07/31/21/16/tent-2561141__340.jpg' }
-]
+}); 
 
 // campgrounds route
 app.get('/campgrounds', function(req, res){
-    res.render('Campgrounds', {campgrounds: campgrounds});
+    //Get all campgrounds from the db
+    Campground.find({}, function(err, allCampgrounds){
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('Campgrounds', {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 // New campground route
@@ -31,9 +56,15 @@ app.post('/campgrounds', function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image:image};
-    campgrounds.push(newCampground);
-    // redirect back to campgrounds page
-    res.redirect('/campgrounds');
+    // create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err) {
+            console.log(err);
+        } else {
+            // redirect back to campgrounds page
+            res.redirect('/campgrounds');
+        }
+    });
 });
 
 // new form route where we can add campgrounds
