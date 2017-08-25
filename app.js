1b86cdddef9e88b259672f7606b08a4c18d8f6f7
Karim <sakjrn@gmail.com>
2017-08-25 16:23:15 +0200

    We now have comment functionality where users can add comments to campgrounds.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Campground = require('./models/campground');
+var Comment = require('./models/comment');
 var seedDB = require('./seeds');
 
 var app = express();
@@ -13,24 +14,11 @@ seedDB();
 mongoose.connect('mongodb://localhost/kiel_camp', ({
     useMongoClient: true
 }));
+mongoose.Promise = global.Promise;
 // bodyparser
 app.use(bodyParser.urlencoded({extended: true}));
 // setup view engine
 app.set('view engine', 'ejs');
-
-// Campground.create(
-//     { 
-//         name: 'Steenbeker Creek', 
-//         image: 'https://cdn.pixabay.com/photo/2013/09/16/19/15/camp-182951__340.jpg',
-//         description: 'this is a beautiful creek, no bathrooms, no water. Beautiful creek!' 
-//     },   function(err, campground){
-//          if(err){
-//         console.log(err);
-//     } else {
-//         console.log('NEWLY CREATED CAMPGROUND: ');
-//         console.log(campground);
-//     }
-// });
 
 // Landing page
 app.get('/', function(req, res){
@@ -44,7 +32,7 @@ app.get('/campgrounds', function(req, res){
         if(err) {
             console.log(err);
         } else {
-            res.render('index', {campgrounds: allCampgrounds});
+            res.render('campgrounds/index', {campgrounds: allCampgrounds});
         }
     });
 });
@@ -69,18 +57,58 @@ app.post('/campgrounds', function(req, res){
 
 // NEW route - show form where we can create campgrounds
 app.get('/campgrounds/new', function(req, res){
-    res.render('new.ejs');
+    res.render('campgrounds/new');
 });
 
 // SHOW route - shows info about a particular campground
 app.get('/campgrounds/:id', function(req, res){
     // find the campground with provided ID
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate('comments').exec(function(err, foundCampground){
         if(err) {
             console.log(err);
         } else {
             // render show template with that campground
-            res.render('show', {campground: foundCampground});
+            res.render('campgrounds/show', {campground: foundCampground});
+        }
+    });
+});
+
+// ==========================
+//     COMMENTS ROUTES
+// ==========================
+
+// Create comment - this shows the form to input comment
+app.get('/campgrounds/:id/comments/new', function(req, res){
+    // find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('comments/new', {campground: campground});
+        }
+    });
+});
+
+// Post comment - this will submit the comment to a campground
+app.post('/campgrounds/:id/comments', function(req, res){
+    // lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect('/campgrounds');
+        } else {
+            // create new comment
+            Comment.create(req.body.comment, function(err, comment){
+                if(err) {
+                    console.log(err);
+                } else {
+                    // connect new comment to campground
+                    campground.comments.push(comment);
+                    campground.save();
+                    // redirect campground showpage
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            });
         }
     });
 });
