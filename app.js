485c118895e08a3dba69792fc6221f309d6b68d5
Karim <sakjrn@gmail.com>
2017-08-28 14:08:10 +0200

    Now working on user authentication. Created a user schema and have done the signup. still have login and signout to go

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
 var Campground = require('./models/campground');
 var Comment = require('./models/comment');
+var User = require('./models/user');
 var seedDB = require('./seeds');
 
 var app = express();
@@ -19,6 +22,18 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 // using seedDB
 seedDB();
+
+// Passport config
+app.use(require('express-session')({
+    secret: 'Anna is still the best',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Landing page
 app.get('/', function(req, res){
@@ -110,6 +125,27 @@ app.post('/campgrounds/:id/comments', function(req, res){
                 }
             });
         }
+    });
+});
+
+// =======================
+// AUTH ROUTES
+// =======================
+// show register form
+app.get('/register', function(req, res){
+    res.render('register');
+});
+// signup logic
+app.post('/register', function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate('local')(req, res, function(){
+            res.redirect('/campgrounds');
+        })
     });
 });
 
