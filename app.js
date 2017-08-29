7754c33dc8e0749a5ec8b4604f2b76ab0cf3e59c
Karim <sakjrn@gmail.com>
2017-08-29 12:20:44 +0200

    Now associated users to comments and campgorunds. Anyone can view campgrounds but only signed up users can add campgrounds and comments

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 // using seedDB
-seedDB();
+// seedDB(); seeding the db
 
 // Passport config
 app.use(require('express-session')({
